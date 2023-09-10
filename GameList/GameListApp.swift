bb39985e665c6b91a5d3981c5280bca373a37438
Jiayi Bian <jiayi@liquidinstruments.com>
2023-09-10 14:45:56 +1000

    Initial Commit

---
+++ GameList/GameListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameListApp.swift
+//  GameList
+//
+//  Created by jiayi_work on 10/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
