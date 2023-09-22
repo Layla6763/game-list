a0029c91078b708b697bdbd080b729dce29eab23
Jiayi Bian <jiayi@liquidinstruments.com>
2023-09-22 17:19:20 +1000

    Complete add game view.

--- GameList/View/AddGameView.swift
+++ GameList/View/AddGameView.swift
@@ -8,13 +8,94 @@
 import SwiftUI
 
 struct AddGameView: View {
+    @Environment (\.managedObjectContext) var managedObjectContext
+    // To take us back to the main view
+    @Environment (\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var platform = 0
+    @State private var releaseYear = 2023
+    @State private var estimateTime = ""
+    @State private var notes = ""
+    @State private var status = 0
+    @State private var ratingScore: Double = 0
+    
+    @State private var isFormSubmitted = false
+    private var platforms = ["Nintendo Switch", "PlayStation 4", "PlayStation 5", "Xbox One", "Xbox Series X/S", "Windows", "MacOS"]
+    
     var body: some View {
         VStack {
-            Text("Add game")
-                .font(.system(size: 30, weight: .heavy))
+            Form {
+                Section(header: Text("New Game").font(.system(size: 25, weight: .bold, design: .default)).foregroundColor(.accentColor)) {
+                    // Game Name
+                    HStack {
+                        Text("Name")
+                        TextField("Game Name", text: $name)
+                            .multilineTextAlignment(.trailing)
+                    }
+                    
+                    // Platform
+                    Picker("Platform", selection: $platform) {
+                        ForEach(0..<platforms.count, id: \.self) {
+                            Text(platforms[$0]).tag($0)
+                        }
+                    }
+                    
+                    // Release Year
+                    Picker("Release Year", selection: $releaseYear) {
+                        ForEach(1900...2100, id: \.self) {
+                            Text(String($0))
+                        }
+                    }
+                    
+                    // Estimate Time
+                    HStack {
+                        Text("Estimate Time")
+                        TextField("0", text: $estimateTime)
+                            .keyboardType(.decimalPad) // Allow decimal input
+                            .multilineTextAlignment(.center)
+                        Text("hours")
+                    }
+                    
+                    // Notes
+                    VStack{
+                        Text("Notes")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        TextEditor(text: $notes)
+                            .frame(minHeight: 200) // Set the minimum height for the text editor
+                            .background(Color(.systemGray5)) // Add a background color
+                            .cornerRadius(8) // Add rounded corners
+                    }
+                    
+                    //Save Button
+                    Section {
+                        Button("Save") {
+                            DataController().addGame(name: name, platform: Int16(platform), releaseYear: Int16(releaseYear), estimateTime: Double(estimateTime) ?? 0, notes: notes, status: Int16(status), ratingScore: ratingScore, context: managedObjectContext)
+                            isFormSubmitted = true
+                        }
+                        .buttonStyle(BorderedButtonStyle()) // Add a border to the button
+                        .frame(maxWidth: .infinity, alignment: .center) // Center the button
+                        .padding(.vertical, 6)
+                    }
+                }
+            }
+            .navigationTitle("Add Game")
+            .alert(isPresented: $isFormSubmitted) {
+                Alert(
+                    title: Text("New Game Saved"),
+                    message: Text("Your game has been saved."),
+                    dismissButton: .default(Text("OK")) {
+                        // Reset the form by clearing the input fields
+                        name = ""
+                        platform = 0
+                        releaseYear = 2023
+                        estimateTime = ""
+                        notes = ""
+                    }
+                )
+            }
             
             ToolbarView()
-                .frame(maxHeight: .infinity, alignment: .bottom)
         }
     }
 }
